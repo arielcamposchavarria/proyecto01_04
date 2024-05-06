c583ac4815b2da8b30d995c4cc82d73396ae8828
DannyJimenez <anjiar03@hotmail.com>
2024-05-05 20:43:45 -0600

    views

--- adminApi/src/App.tsx
+++ adminApi/src/App.tsx
@@ -1,14 +1,24 @@
-import TableProducts from "./components/TableProducts";
-
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './views/Home'
+import Create from './views/Create'
+import Update from './views/Update'
+import Read from './views/Read'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
 
 function App() {
 
-  return (
-      <> 
-      <TableProducts/>
-      </>
 
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path= '/' element={<Home />}></Route>
+        <Route path= '/create' element={<Create />}></Route>
+        <Route path= '/update/:id' element={<Update />}></Route>
+        <Route path= '/read/:id' element={<Read />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
