cd83df6dd03afe6e6e4d33b2ed142b52eb5e3a6d
arielcamposchavarria <128259451+arielcamposchavarria@users.noreply.github.com>
2024-05-05 20:17:16 -0600

    Revert "Views"

--- adminApi/src/App.tsx
+++ adminApi/src/App.tsx
@@ -1,25 +1,14 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './views/Home'
-import Create from './views/Create'
-import Update from './views/Update'
-import Read from './views/Read'
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import TableProducts from "./components/TableProducts";
 
 
 function App() {
 
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path= '/' element={<Home />}></Route>
-        <Route path= '/create' element={<Create />}></Route>
-        <Route path= '/update/:id' element={<Update />}></Route>
-        <Route path= '/read/:id' element={<Read />}></Route>
-      </Routes>
-    </BrowserRouter>
+      <> 
+      <TableProducts/>
+      </>
+
   )
 }
 
-export default App
+export default App;
