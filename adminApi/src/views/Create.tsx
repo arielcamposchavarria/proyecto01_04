657ee7b97f605487b9d569e6000c7418586cd3c5
arielcamposchavarria <campos2004.05@gmail.com>
2024-05-10 12:01:16 -0600

    changeapi

--- adminApi/src/views/Create.tsx
+++ adminApi/src/views/Create.tsx
@@ -5,21 +5,21 @@ import { Link, useNavigate } from 'react-router-dom'
 
 function Create() {
     const [values, setValues] = useState({
-        title: '',
+        name: '',
         price: '',
         description: '',
         category: '',
-        image: ''
+        imagen: ''
     })
 
     const navigate = useNavigate();
     const handleSubmit = (event: { preventDefault: () => void; }) => {
-        event.preventDefault();
-        axios.post('https://fakestoreapi.com/products', values)
+        //event.preventDefault();
+        axios.post('https://663e4425e1913c4767971f9e.mockapi.io/articulos', values)
         .then(res => {
             setValues(res.data);
 
-            console.log(res);
+            //console.log(res);
             navigate('/')
         })
         .catch(err => console.log(err))
@@ -32,7 +32,7 @@ function Create() {
             <div className='mb-2'>
                 <label htmlFor="title">Title</label>
                 <input type="text" name='title' className='form-control' placeholder='Enter title'
-                onChange={e => setValues({...values, title: e.target.value}) }/>
+                onChange={e => setValues({...values, name: e.target.value}) }/>
             </div>
             <div className='mb-2'>
                 <label htmlFor="price">Price</label>
@@ -52,7 +52,7 @@ function Create() {
             <div className='mb-2'>
                 <label htmlFor="image">Image</label>
                 <input type="text" name='image' className='form-control' placeholder='Enter Image Url'
-                onChange={e => setValues({...values, image: e.target.value}) }/>
+                onChange={e => setValues({...values, imagen: e.target.value}) }/>
             </div>
             <button className='btn btn-succes'>Submit</button>
             <Link to='/' className='btn btn-primary ms-3'>Back</Link>
